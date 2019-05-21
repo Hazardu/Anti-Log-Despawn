1fff882318dea57b53155ce18fab641dabc18c32
Hazardu <42750285+Hazardu@users.noreply.github.com>
2019-05-21 18:02:58 +0200

    Created

---
+++ LogManagerExtended.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Anti_Log_Despawn
+{
+    class SceneTrackerEx : sceneTracker 
+    {
+        protected override void doAwake()
+        {
+            base.doAwake();
+            maxSpawnedLogs = int.MaxValue - 8;
+        }
+    }
+}
